973302f35ec82ebce9daedb633925350d28dbb85
Dhanush1509 <s.munidhanush15@gmail.com>
2021-05-23 21:58:32 +0530

    clean & deploy

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,24 @@ import { notFound, errorHandler } from "./middlewares/errorHandler.js";
 import users from "./routes/users.js";
 import dotenv from "dotenv";
 import CONNECT_DB from "./config/db.js";
+import path from "path"
 dotenv.config();
 
 const app = express();
 CONNECT_DB();
 app.use(express.json());
 app.use("/api/users", users);
-app.get("/",(req, res) =>{res.json({"message":"api is running"})})
-
-
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
